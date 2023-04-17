e1aeccf319c41a1af49b246bfbbfb92aa275d217
William <william.debotte77@gmail.com>
2023-04-17 17:54:44 +0200

    Save work on wordpress

---
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+while ! mysqladmin ping -h"127.0.0.1" --silent; do
+	echo "[MySQL] Waiting for database connection..."
+	sleep 1
+done
+
+echo "[MySQL] Service has started!"
+
+# Avoir PHP running problems
+if [ ! -d "/run/php" ]; then
+	mkdir -p /run/php
+	touch /run/php/php7.3-fpm.pid
+fi
+
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+	echo "[Wordpress] The installation will start...";
+
+	wp core download --allow-root
+	wp config create --allow-root \
+					--dbname=$SQL_DATABASE_NAME \
+					--dbuser=$SQL_USER \
+					--dbpass=$SQL_PASSWORD \
+					--dbhost=mariadb:3306 --path='/var/www/html'
+
+	echo "[Wordpress] End of install..."
+fi
+
+/usr/sbin/php-fpm7.3
