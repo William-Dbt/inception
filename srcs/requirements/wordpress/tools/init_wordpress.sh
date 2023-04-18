3cb0f56720e0174773cd13dc5bf18606eacc3ec3
William <william.debotte77@gmail.com>
2023-04-18 15:13:24 +0200

    Wordpress containe seems to be done, have to check with docker-compose to avoir error of mariadb

--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-while ! mysqladmin ping -h"127.0.0.1" --silent; do
-	echo "[MySQL] Waiting for database connection..."
-	sleep 1
-done
-
+# while ! mysqladmin ping -h"mariadb:3306" --silent; do
+# 	echo "[MySQL] Waiting for database connection..."
+# 	sleep 1
+# done
 echo "[MySQL] Service has started!"
 
 # Avoid PHP running problems
@@ -13,17 +12,43 @@ if [ ! -d "/run/php" ]; then
 	touch /run/php/php7.3-fpm.pid
 fi
 
-if [ ! -f "/var/www/html/wp-config.php" ]; then
-	echo "[Wordpress] The installation will start...";
-
+# Download wordpress if isn't already done
+if [ ! -f "wp-config-sample.php" ]; then
+	echo "[Wordpress] Downloading wordpress..."
 	wp core download --allow-root
-	wp config create --allow-root \
-					--dbname=$SQL_DATABASE_NAME \
-					--dbuser=$SQL_USER \
-					--dbpass=$SQL_PASSWORD \
-					--dbhost=mariadb:3306 --path='/var/www/html'
+	echo "[Wordpress] Done!"
+fi
 
-	echo "[Wordpress] End of install..."
+# Check if the configuration of wordpress is already done, if not, start the configuration and installation
+# https://make.wordpress.org/cli/handbook/how-to/how-to-install/
+if [ ! -f "wp-config.php" ]; then
+	echo "[Wordpress] Generate config file and create database..."
+	wp config create --dbname=$SQL_DATABASE_NAME \
+					 --dbuser=$SQL_USER \
+					 --dbpass=$SQL_PASSWORD \
+					 --dbhost=mariadb:3306 \
+					 --allow-root
+	wp db create --allow-root
+	echo "[Wordpress] Done!"
+
+	echo "[Wordpress] Installing wordpress..."
+	wp core install --url="wdebotte.42.fr" \
+					--title="Inception" \
+					--admin_user=$WP_ADMIN_USERNAME \
+					--admin_password=$WP_ADMIN_PASSWORD \
+					--admin_email=$WP_ADMIN_EMAIL \
+					--allow-root
+	echo "[Wordpress] Done!"
+fi
+
+# Create another user (admin user already exists thanks to the wp core install)
+# https://developer.wordpress.org/cli/commands/user/
+if [ ! $(wp user get $WP_USER_USERNAME --allow-root) ]; then
+	echo "[Wordpress] Creating new user ($WP_USER_USERNAME)"
+	wp user create $WP_USER_USERNAME $WP_USER_EMAIL \
+				   --user_pass=$WP_USER_PASSWORD \
+				   --allow-root
+	echo "[Wordpress] Done!"
 fi
 
 /usr/sbin/php-fpm7.3
